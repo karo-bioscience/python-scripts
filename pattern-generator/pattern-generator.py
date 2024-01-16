af99333f21b5302695fa02633995f1558ec73519
Karolina Sikorska <156694011+karo-bioscience@users.noreply.github.com>
2024-01-16 20:56:27 +0100

    Update pattern-generator.py

--- pattern-generator/pattern-generator.py
+++ pattern-generator/pattern-generator.py
@@ -1,20 +1,16 @@
-import sys
-
 n = int(input('Enter value of a natural number: '))
 x = ''
-m = n
-
-if n <= 0:
-   x = 'Invalid number. Given value will not create a pattern'
-   sys.exit(x)
-
-while n > 0:
-   x+= (n * ' * ') + '\n'
-   n -= 1
-
+#Saving the natural number in different variable
+m = n 
 end = 'Pattern made from entered natural number ({0})'
 
-print(x, end.format(m))
-
+if n <= 0:
+    x = 'Invalid number. Given value will not create a pattern'
+    print(x)
+else:
+    while n > 0:
+        x+= (n * ' * ') + '\n'
+        n -= 1
+    print(x, end.format(m))
 
 input("Press ENTER to exit")
